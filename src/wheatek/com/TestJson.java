584eeabd69c85703ccefc2b01fed5fb714e9f5c4
zry520 <33924493+zry520@users.noreply.github.com>
2017-11-28 19:09:35 +0800

    提交信息

---
+++ src/wheatek/com/TestJson.java
@@ -0,0 +1,10 @@
+package wheatek.com;
+
+public class TestJson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello world");
+	}
+
+}
